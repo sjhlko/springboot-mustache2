da9217c9276789e8ff95b755e28dba276aac0972
서정희 <sjhlko@naver.com>
2022-11-10 11:33:38 +0900

    Initial commit

---
+++ src/main/java/com/mustache/springbootmustache2/SpringbootMustache2Application.java
@@ -0,0 +1,13 @@
+package com.mustache.springbootmustache2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMustache2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMustache2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mustache/springbootmustache2/SpringbootMustache2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustache.springbootmustache2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMustache2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
